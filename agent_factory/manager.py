2f432ad79875a5a11fb373dfa5c51c18fe4c5c0d
罗钦雨 <1820851049@qq.com>
2024-11-11 16:04:45 -0500

    add: basic decomposition chat to AgentManager

--- agent_factory/manager.py
+++ agent_factory/manager.py
@@ -5,17 +5,20 @@
 #   • Task assignment: Preparing the sub-tasks for the subsequent AgentGenerator to generate corresponding Agents.
 
 from config import read_config
-from utils import read_prompt_template
 import os
+
+from jinja2 import Environment, FileSystemLoader
+from utils import call_llm
 
 class Manager:
     def __init__(self):
         self.original_task = ''
         self.sub_tasks = []
         self.config = read_config()
-        self.model = self.config.get('default_model')
+        self.model = self.config.get('default_model',"gpt-4o-mini")
+        self.env = Environment(loader=FileSystemLoader(self.config.get('prompt_folder_path')))
         self.sys_prompt = ''
-        self.breakdown_prompt = read_prompt_template(os.path.join(self.config.get('prompt_folder_path'), 'manager_break_down.txt'))
+        self.breakdown_prompt = self.env.get_template('manager_break_down.txt')
         self.reflection_prompt = None
         self.repeat_turns = 10
 
@@ -25,16 +28,24 @@ class Manager:
         接收原始任务。
         """
         self.original_task = task
-        print(f"Manager received task: {self.original_task}")
 
     def breakdown_task(self):
         """
         将任务拆解成子任务。
         """
+        data = {
+            'task': self.original_task
+        }
+        rendered_prompt = self.breakdown_prompt.render(**data)
+        response_message = call_llm(model=self.model,sys_prompt=self.sys_prompt,usr_prompt=rendered_prompt,config=self.config)
+        print(response_message)
+
 
 manager = Manager()
-manager.receive_task("I want to book a flight ticket.")
 
-print(manager.breakdown_prompt)
+task = """Assuming scientists in the famous youtube video The Thinking Machine (Artificial Intelligence in the 1960s) were interviewed the same year, what is the name of the scientist predicting the sooner thinking machines or robots? Answer using the format First name Last name"""
+manager.receive_task(task)
+manager.breakdown_task()
 
-print(manager.config)       
+
+
