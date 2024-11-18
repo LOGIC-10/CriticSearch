fbb3a1eee0e62f96157c1d819636a4a07fedc2ca
罗钦雨 <1820851049@qq.com>
2024-11-17 21:18:23 -0500

    add: accomplish more on critic part

---
+++ critic_agent/critic_agent.py
@@ -0,0 +1,69 @@
+import json
+import re
+import sys
+import os
+
+import sys
+from pathlib import Path
+
+import yaml
+
+project_root = Path(__file__).resolve().parents[1]  # 获取项目根目录
+sys.path.append(str(project_root / 'agent_factory'))  # 添加到搜索路径中
+sys.path.append(str(project_root / 'critic_agent'))
+
+custom_module_path = '/Users/logic/Documents/CodeSpace/AgentFactory'
+if custom_module_path not in sys.path:
+    sys.path.append(custom_module_path)
+
+print(sys.path)
+
+from agent_factory.agent import BaseAgent
+
+
+class CriticAgent(BaseAgent):
+    def __init__(self):
+        super().__init__()
+        self.original_task = ''
+        self.critic_prompt = self.env.get_template('critic_agent.txt')
+
+    def critic(self):
+        """
+        生成评论。
+        """
+        data = self.get_data_for_critic()
+        model_response = self.chat_with_template(data, self.critic_prompt)
+
+        try:
+            formatted_yaml = self.extract_and_validate_yaml(model_response)
+            return formatted_yaml
+        
+        except yaml.YAMLError as exc:
+            print(f"Invalid YAML content: {exc}")
+            return None
+    
+    def receive_agent_answer(self, agent_answer):
+        self.agent_answer = agent_answer
+
+    def get_data_for_critic(self):
+        return {
+            'user_question': self.original_task,
+            'agent_answer': self.agent_answer
+        }
+
+
+    
+task = """why do we say google is facing chaleenges in 2019?"""
+
+common_agent = BaseAgent()
+common_agent_answer = common_agent.common_chat(query=task)
+print(f"common_agent_answer:\n\n{common_agent_answer}\n\n")
+CriticAgent = CriticAgent()
+CriticAgent.receive_task(task)
+CriticAgent.receive_agent_answer(common_agent_answer)
+
+response_message = CriticAgent.critic()
+# print(f"Comment:\n\n{response_message}")
+
+
+print(response_message)
